e3a76ecc7d777d3c5a8f23fc57ee9ec90c5357b0
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-04-26 23:54:18 +0530

    register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:zencash/auth/login.dart';
 import 'package:zencash/onbard/dot.dart';
 import 'package:zencash/onbard/page_one.dart';
 import 'package:zencash/onbard/splash.dart';
@@ -20,7 +21,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Splash(),
+      home: LoginPage(),
     );
   }
 }
